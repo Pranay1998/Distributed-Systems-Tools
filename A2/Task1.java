f26a4c961369714ba1f44a405834b66dadd7edd1
Pranay Reddy Katta <preddyka@uwaterloo.ca>
2020-07-01 18:45:06 +0530

    A2

---
+++ A2/Task1.java
@@ -0,0 +1,75 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+
+public class Task1 {
+
+  public static class MaxUserMapper extends Mapper<Object, Text, Text, Text>{
+    private final static IntWritable one = new IntWritable(1);
+    private Text movie = new Text();
+    private Text highestUsers = new Text();
+      
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] tokens = value.toString().split(",", -1);
+
+      movie.set(tokens[0]);
+
+      char max = '0';
+
+      StringBuilder sb = new StringBuilder();
+      for (int i = 1; i < tokens.length; i++) {
+        if (tokens[i].length() > 0) {
+          char t = tokens[i].charAt(0);
+          if (t > max) {
+            max = t;
+            sb.setLength(0);
+            sb.append(i);
+          } else if (t == max) {
+            sb.append(',');
+            sb.append(i);
+            
+          }
+        }
+      }
+
+      highestUsers.set(sb.toString());
+      context.write(movie, highestUsers);
+    }
+  }
+    
+    
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    conf.set("mapreduce.output.textoutputformat.separator", ",");
+
+    String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+    if (otherArgs.length != 2) {
+      System.err.println("Usage: wordcount <in> <out>");
+      System.exit(2);
+    }
+
+    Job job = Job.getInstance(conf, "Task1");
+    job.setJarByClass(Task1.class);
+    
+    job.setMapperClass(MaxUserMapper.class);
+    job.setNumReduceTasks(0);
+    
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    
+    TextInputFormat.addInputPath(job, new Path(otherArgs[0]));
+    TextOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+    
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
